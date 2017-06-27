f353f2c82b1535487f9d8715dde02141291a0df0
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-06-27 12:03:19 -0400

    Finish POST

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -21,4 +21,43 @@ router.get('/products', (req, res, next) => {
 });
 
 
+// STEP #1 of form submission for a new product
+router.get('/products/new', (req, res, next) => {
+  // display "new-product-view.ejs"
+  res.render('product-views/new-product-view.ejs');
+});
+
+// STEP #2 of form submission for a new product
+// <form method="post" action="/products">
+//                |                |
+//      -----------                |
+//      |        -------------------
+//      |        |
+router.post('/products', (req, res, next) => {
+    const theProduct = new ProductModel({
+        name: req.body.productName,
+        price: req.body.productPrice,
+        imageUrl: req.body.productImageUrl,
+        description: req.body.productDescription
+    });
+
+    theProduct.save((err) => {
+        if (err) {
+          // If there was an error, use next() to skip to the ERROR PAGE.
+          next(err);
+          return;
+        }
+
+        // If saved successfully, redirect to a URL.
+        // (redirect is STEP #3 of form submission for a new product)
+        res.redirect('/products');
+          // you can ONLY redirect to a URL 🌏
+
+          // 🚨🚨🚨
+          // If you don't redirect, you can refresh and duplicate your data!
+          // 🚨🚨🚨
+    });
+});
+
+
 module.exports = router;
