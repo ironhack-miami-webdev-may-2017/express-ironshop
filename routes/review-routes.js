7656122e339b63f996a28e5c638c704c735f1b37
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-06-28 12:00:53 -0400

    Add review POST

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const ProductModel = require('../models/product-model.js');
+const ReviewModel = require('../models/review-model.js');
 
 const router = express.Router();
 
@@ -30,6 +31,42 @@ router.get('/products/:productId/reviews/new', (req, res, next) => {
 
 
 // ROUTE #2 -> receive that form submission and do database stuff
+  // POST to /products/9999999999/reviews
+router.post('/products/:productId/reviews', (req, res, next) => {
+
+    ProductModel.findById(
+      req.params.productId,      // 1st argument -> id of the product
+
+      (err, productFromDb) => {  // 2nd argument -> callback
+          if (err) {
+            // use next() to skip to the ERROR PAGE
+            next(err);
+            return;
+          }
+
+          const theReview = new ReviewModel({
+            author: req.body.reviewAuthor,
+            stars: req.body.reviewStars,
+            content: req.body.reviewContent
+          });
+
+          // Add the review to the product's "reviews" array
+          productFromDb.reviews.push(theReview);
+
+          // Save the product with the new review
+          productFromDb.save((err) => {
+              if (err) {
+                // use next() to skip to the ERROR PAGE
+                next(err);
+                return;
+              }
+
+              res.redirect('/products/' + productFromDb._id);
+          });
+      }
+    ); // end findById
+
+});  // end POST route
 
 
 module.exports = router;
